3eec0a56dcce416291da83b44235e84acf339974
F93AB7X <joseph.schmitt1@fiserv.com>
2023-10-26 16:16:33 -0400

    Pushed Try it yourslf exercises

---
+++ src/Inheritance/Abstraction/Truck.java
@@ -0,0 +1,18 @@
+package Inheritance.Abstraction;
+
+public class Truck extends Vehicle {
+    @Override
+	int gasTankCapacity() {
+		return 50;
+	}
+
+	@Override
+	int getTopSpeed() {
+		return 100;
+	}
+
+	@Override
+	String getVehicleName() {
+		return "Chevy Silverado";
+	}
+}
